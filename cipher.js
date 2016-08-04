9616073a7d61a6d1929641704fede0546c952709
Robert Diamond <rmd6502@gmail.com>
2016-08-04 13:58:41 -0700

    make it more readable

--- cipher.js
+++ cipher.js
@@ -1,8 +1,17 @@
 const process = require('process');
 
 const re = new RegExp('[^A-Za-z]','g');
-const key = process.argv[2].replace(re, '').toUpperCase();
-const message = process.argv[3].replace(re, '').toUpperCase();
+let base = 2;
+let direction = 1;
+while (process.argv[base][0] === '-') {
+    ++base;
+    switch(process.argv[base][1]) {
+        case 'e': direction = -1; break;
+        case 'd': direction = 1; break;
+    }
+}
+const key = process.argv[base].replace(re, '').toUpperCase();
+const message = process.argv[base + 1].replace(re, '').toUpperCase();
 
 const charA = 'A'.charCodeAt(0);
 const keyArray = [];
@@ -13,7 +22,10 @@ for (let i=0; i < key.length; ++i) {
 const decrypted = [];
 for (let i=0; i < message.length; ++i) {
     // +26 keeps negative numbers from generating weird character codes
-    decrypted.push(((message.charCodeAt(i) - charA + keyArray[i % keyArray.length] + 26) % 26) + charA);
+    const messageChar = message.charCodeAt(i) - charA;
+    const keyChar = keyArray[i % keyArray.length];
+    const decryptedChar = ((messageChar + direction * keyChar + 26) % 26) + charA;
+    decrypted.push(decryptedChar);
 }
 // apply goes from an array to a ... list of arguments
 console.log('decrypted '+String.fromCharCode.apply(String, decrypted));
